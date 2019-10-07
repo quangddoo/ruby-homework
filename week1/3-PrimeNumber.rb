7daab91cc287d9bfa17e80275f0f2128fd44d094
quangddoo <36820098+quangddoo@users.noreply.github.com>
2019-10-07 21:50:49 +0700

    update format

--- week1/3-PrimeNumber.rb
+++ week1/3-PrimeNumber.rb
@@ -1,17 +1,17 @@
 def is_prime(number)
-	return false if number <2
-	(2..Math.sqrt(number)).each do |divisor|
-		return false if number % divisor == 0
-	end
-	return number
+  return false if number <2
+  (2..Math.sqrt(number)).each do |divisor|
+    return false if number % divisor == 0
+  end
+  return number
 end
 
 def primes(array)
-    primes_array = []
-  	array.each do |num|
+  primes_array = []
+  array.each do |num|
     primes_array.push(num) if is_prime(num)
-	end
-	return primes_array
+  end
+  return primes_array
 end
 
 arr = [*0..100]
